976c609a04ab6c8aa6990bcf9acb3b836eba2dc5
CarreresBastien <carreresbastien@gmail.com>
2018-03-22 07:54:20 +0100

    Ajout de l'écriture dans un fichier

--- pretraitement.py
+++ pretraitement.py
@@ -5,11 +5,13 @@ import csv
 import unicodedata
 
 with open("./logstash-6.2.2/input.tsv",encoding ='utf-8') as f:
-	reader = csv.reader(f, delimiter = '\t')
-	for ligne in reader:
-		for reponse in ligne:
-			# for reponse in reponses
-			reponse = unicodedata.normalize('NFD', reponse).encode('ascii', 'ignore').decode('ASCII')
-			reponse = reponse.replace(" \\t","\\t")
-			print(reponse) 
-			#print(reponse)
+	with open("./logstash-6.2.2/input_processed.tsv",encoding='utf-8') as f_out:
+		reader = csv.reader(f, delimiter = '\t')
+		for ligne in reader:
+			for reponse in ligne:
+				reponse = unicodedata.normalize('NFD', reponse).encode('ascii', 'ignore').decode('ASCII')
+				reponse = reponse.replace(" \\t","\\t")
+				print(reponse) 
+
+
+	#TODO reecrire les modifs dans un fichier .csv
