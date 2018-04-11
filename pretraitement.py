9fa43b04c8120cd279c056d0d6be47c6e221c38a
Myastro <dumon.leo@gmail.com>
2018-04-11 10:25:13 +0200

    add subjectivity and polarity

--- pretraitement.py
+++ pretraitement.py
@@ -9,9 +9,32 @@ from elasticsearch import *
 import nltk
 nltk.download('punkt')
 
-import textblob as tb
+from textblob import TextBlob
 
 es = Elasticsearch()
+
+def return_sentences_polarity_subjectivity(ligne):
+
+    blb = TextBlob(ligne)
+    try:
+        blb = blb.translate(to='en')
+    except:
+        blb = TextBlob(reponse)
+
+    sumPolarity = 0
+    sumSubjectivity = 0
+
+    for sentences in blb.sentences:
+        sumPolarity += sentences.sentiment.polarity
+        sumSubjectivity += sentences.sentiment.subjectivity
+
+    to_return = {
+        'response': ligne,
+        'polarity': sumPolarity/len(blb.sentences),
+        'subjectivity': sumSubjectivity/len(blb.sentences)
+    }
+
+    return to_return
 
 # f_out =open("./logstash-6.2.2/input_processed.tsv",'w',encoding = 'utf-8')
 with open("./input.tsv", encoding='utf-8') as f:
@@ -21,31 +44,64 @@ with open("./input.tsv", encoding='utf-8') as f:
     num_ligne = 0
     for ligne in reader:
         for i, reponse in enumerate(ligne):
-            # reponse = unicodedata.normalize('NFD', reponse).encode('ascii', 'ignore').decode('ASCII')
+
             reponse = reponse.replace(" \\t", "\\t")
+            blb = TextBlob(reponse)
+
+            try:
+                blb = blb.translate(to='en')
+            except:
+                blb = TextBlob(reponse)
+
             ligne[i] = reponse
         ligne = ligne[0].split('\\t')
+        for index in range(len(ligne)):
+                ligne[index] = return_sentences_polarity_subjectivity(ligne[index])
         print(ligne)
         if num_ligne != 0:
-            es.create(index='raiponce', doc_type='response', id=num_ligne, body=
+            es.create(index='raiponce', doc_type='_doc', id=num_ligne, body=
             {
-                'House': ligne[0],
-                'Cooking': ligne[1],
-                'Music': ligne[2],
-                'Game': ligne[3],
-                'Sport': ligne[4],
-                'Phone': ligne[5],
-                'DigitalService': ligne[6],
-                'Gender': ligne[7],
-                'Age': ligne[8],
-                'City': ligne[9],
-                'Date': ligne[10],
-                'UserId': ligne[11]
+                'House': {
+                    'response': ligne[0]['response'],
+                    'polarity': ligne[0]['polarity'],
+                    'subjectivity': ligne[0]['subjectivity']
+                },
+                'Cooking':{
+                    'response': ligne[1]['response'],
+                    'polarity': ligne[1]['polarity'],
+                    'subjectivity': ligne[1]['subjectivity']
+                },
+                'Music': {
+                    'response': ligne[2]['response'],
+                    'polarity': ligne[2]['polarity'],
+                    'subjectivity': ligne[2]['subjectivity']
+                },
+                'Game': {
+                    'response': ligne[3]['response'],
+                    'polarity': ligne[3]['polarity'],
+                    'subjectivity': ligne[3]['subjectivity']
+                },
+                'Sport': {
+                    'response': ligne[4]['response'],
+                    'polarity': ligne[4]['polarity'],
+                    'subjectivity': ligne[4]['subjectivity']
+                },
+                'Phone': {
+                    'response': ligne[5]['response'],
+                    'polarity': ligne[5]['polarity'],
+                    'subjectivity': ligne[5]['subjectivity']
+                },
+                'DigitalService': {
+                    'response': ligne[6]['response'],
+                    'polarity': ligne[6]['polarity'],
+                    'subjectivity': ligne[6]['subjectivity']
+                },
+                'Gender': ligne[7]['response'],
+                'Age': ligne[8]['response'],
+                'City': ligne[9]['response'],
+                'Date': ligne[10]['response'],
+                'UserId': ligne[11]['response']
             })
         num_ligne += 1
 
-# data_writer = csv.writer(f_out,delimiter = '\t')
-# 		#data_writer.writerow(ligne)
-# with open('input_json.txt','w') as f_out:
-# 	json.dump(data,f_out)
-# f_out.close()
+
